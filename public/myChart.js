0a15a0d3baf8534c0f5f9fed27cb9de1db59d8a2
Mikko Kotola <mikko.kotola@sympa.com>
2020-02-02 19:25:47 +0200

    Fetch countryinfo by code

--- public/myChart.js
+++ public/myChart.js
@@ -6,7 +6,7 @@ function renderChart(data, labels) {
         // Clear the previous chart if it exists
         currentChart.destroy();
     }
-    
+
     // Draw new chart
     currentChart = new Chart(ctx, {
         type: 'line',
@@ -19,16 +19,33 @@ function renderChart(data, labels) {
                 backgroundColor: 'rgba(75, 192, 192, 0.2)',
             }]
         },
-        options: {            
+        options: {
             scales: {
                 yAxes: [{
                     ticks: {
-                        beginAtZero: true                        
+                        beginAtZero: true
                     }
-                }]                
+                }]
             }
         },
     });
+}
+
+function renderCountryInfo(countryName, capital, region, flagUrl) {
+    // Create an image element
+    var img = document.createElement('img'); 
+    img.src = flagUrl;
+    img.id = 'flag';
+    img.alt='Country flag';
+
+    // Insert the texts and image element into the HTML document
+    document.getElementById('countryName').innerHTML = countryName;
+    document.getElementById('capital').innerHTML = 'Capital: ' + capital;
+    document.getElementById('region').innerHTML = 'Region:' + region;
+    if (document.getElementById('flagcontainer').firstChild !== null) {
+        document.getElementById('flagcontainer').firstChild.remove();
+    }    
+    document.getElementById('flagcontainer').appendChild(img);
 }
 
 function getValues(data) {
@@ -42,41 +59,49 @@ function getLabels(data) {
 }
 
 function validateCountryCode(input) {
-    if(input.match(/^[a-zA-Z]+$/)){
+    if (input.match(/^[a-zA-Z]+$/)) {
         return true;
     }
-    else{
+    else {
         return false;
     }
 }
 
 function validateIndicator(input) {
-    if(input.match(/^[a-zA-Z]+.[a-zA-Z]+.[a-zA-Z]+$/)){
+    if (input.match(/^[a-zA-Z]+.[a-zA-Z]+.[a-zA-Z]+$/)) {
         return true;
     }
-    else{
+    else {
         return false;
     }
 }
 
-function renderError(errorMessage) {
-    document.getElementById('errorbox').innerHTML = errorMessage;
-    setTimeout(() => document.getElementById('errorbox').innerHTML = '', 5000);
-}
-
-function fetchDataAndRenderGraph(countryCode, indicatorCode) {
+function validateInput(countryCode, indicatorCode) {
     if (!validateCountryCode(countryCode)) {
         console.log('Invalid countryCode: ' + countryCode)
         renderError('Country code malformed. Valid example: FIN');
         return;
     }
-
+    
     if (!validateIndicator(indicatorCode)) {
         console.log('Invalid indicatorCode: ' + indicatorCode)
         renderError('Indicator code malformed. Valid example: SP.POP.TOTL');
         return;
     }
-    
+}
+
+
+function renderError(errorMessage) {
+    document.getElementById('errorPopulationData').innerHTML = errorMessage;
+    setTimeout(() => document.getElementById('errorPopulationData').innerHTML = '', 5000);
+}
+
+function renderCountryDataError(errorMessage) {
+    document.getElementById('errorCountryData').innerHTML = errorMessage;
+    setTimeout(() => document.getElementById('errorCountryData').innerHTML = '', 5000);
+}
+
+async function fetchDataAndRenderGraph(countryCode, indicatorCode) {
     // Function in Google cloud
     const baseUrl = 'https://europe-west1-world-bank-data.cloudfunctions.net/world-bank-fetcher/country/'
     const url = baseUrl + countryCode + '/indicator/' + indicatorCode
@@ -86,45 +111,89 @@ function fetchDataAndRenderGraph(countryCode, indicatorCode) {
     fetch(url, {
         method: 'GET',
         headers: {
-          'Content-Type': 'application/json'          
-        }
-      })
-      .then(async function(response) {
-        if (response.status == 422) {
-            renderError('Malformed country code');
-        } 
-        else if (!response.ok) {
-            throw Error(response.statusText);
-        } 
-        else {
-            if (response.status == 200) {
-                var dataAsString = await response.json();
-                fetchedData = JSON.parse(dataAsString)
-    
-                if (fetchedData[0].message) {
-                    throw(JSON.stringify(fetchedData[0].message));
-                }
-
-                var data = getValues(fetchedData);
-                var labels =  getLabels(fetchedData);
-                renderChart(data, labels);
-            } else {
-                renderError('Fetching population data from server failed');
-            }
+            //'Content-Type': 'application/json'
+            'Content-Type': 'text/plain'
         }
     })
-    .catch(function(error) {
-        console.log(error)
-        renderError('Fetching population data from server failed. ' + error);
-    }); 
+        .then(async function (response) {
+            if (response.status == 422) {
+                renderError('Malformed country code');
+            }
+            else if (!response.ok) {
+                throw Error(response.statusText);
+            }
+            else {
+                if (response.status == 200) {
+                    var dataAsString = await response.json();
+                    fetchedData = JSON.parse(dataAsString)
+                    
+                    if (fetchedData[0].message) {
+                        throw (JSON.stringify(fetchedData[0].message));
+                    }
+
+                    var data = getValues(fetchedData);
+                    var labels = getLabels(fetchedData);
+                    renderChart(data, labels);
+                } else {
+                    renderError('Fetching population data from server failed');
+                }
+            }
+        })
+        .catch(function (error) {
+            console.log(error)
+            renderError('Fetching population data from server failed. ' + error);
+        });
 }
 
-function renderCountryGraph () {
+async function fetchDataAndRenderCountryInfo(countryCode) {
+    // REST Countries API
+    const baseUrl = 'https://restcountries.eu/rest/v2/name/';
+    const url = baseUrl + countryCode;
+    
+    fetch(url, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'text/plain'
+        }
+    })
+        .then(async function (response) {
+            if (response.status == 422) {
+                renderError('Malformed country code');
+            }
+            else if (!response.ok) {
+                throw Error(response.statusText);
+            }
+            else {
+                if (response.status == 200) {
+                    //var dataAsString = await response.json();
+                    var countryData = await response.json();
+                    //var countryData = JSON.parse(dataAsString);
+
+                    var countryName = countryData[0].name;
+                    var countryCapital = countryData[0].capital;
+                    var countryRegion = countryData[0].region;
+                    var countryFlagUrl = countryData[0].flag;
+                    
+                    renderCountryInfo(countryName, countryCapital, countryRegion, countryFlagUrl);
+                } else {
+                    renderCountryDataError('Fetching country data from RESTCountries API failed');
+                }
+            }
+        })
+        .catch(function (error) {
+            console.log(error)
+            renderError('Fetching population data from server failed. ' + error);
+        });
+}
+
+function renderCountryView() {
     var country = document.getElementById('country').value;
     const indicator = 'SP.POP.TOTL';
+    validateInput(country, indicator);
     fetchDataAndRenderGraph(country, indicator);
+    fetchDataAndRenderCountryInfo(country);
 }
 
-document.getElementById('renderBtn').addEventListener('click', renderCountryGraph);
+document.getElementById('renderBtn').addEventListener('click', renderCountryView);
 
 document.getElementById('country').focus();
